aac375e11b278d035e8f33c597117977db70d690
ggdevlaptop <gonzalo.a.gomez22@gmail.com>
2025-01-22 22:01:00 -0300

    Style genres view.

--- controller/genresController.js
+++ controller/genresController.js
@@ -4,6 +4,14 @@ async function getAllGenres(req, res) {
   try {
     const genres = await db.getGenres();
     // console.log("Genres: ", genres);
+    let newString = "";
+    genres.map((genre) => {
+      const word = genre.genres;
+      // const newString = genre.slice(0, 1);
+      newString = word.slice(0, 1).toUpperCase() + word.slice(1);
+      return newString;
+    });
+
     res.render("genres", { title: "Genres", genres, regExpFunction });
   } catch (error) {
     console.error("Error fetching genres: ", error);
